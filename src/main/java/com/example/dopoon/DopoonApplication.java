d376160c21babe5c8d4e3e422948a63a2af77b5e
허윤 <huryoun5022@naver.com>
2026-08-17 20:27:30 +0900

    Initial project setup: Spring Boot + Lombok + JPA + H2

---
+++ src/main/java/com/example/dopoon/DopoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.dopoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dopoon/DopoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dopoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
